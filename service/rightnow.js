3777458f2fc638b700e3358bdf813b7e8617abbd
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-04-24 15:52:01 +0900

    try adding filter on getSmartAssistantSearch

--- service/rightnow.js
+++ service/rightnow.js
@@ -70,11 +70,13 @@ module.exports = class RightNow {
                             Body: question,
                             Subject: question,
                             Limit: 1,
-                            Filters: [{
-                                ServiceProduct: {
-                                    Name: "LGBT"
-                                }
-                            }]
+                            Filters: {
+                                ContentFilterList: [{
+                                    ServiceProduct: {
+                                        Name: "LGBT"
+                                    }
+                                }]
+                            }
                         }, function(err, result){
                             if (err){
                                 debug("Failed to serach.");
