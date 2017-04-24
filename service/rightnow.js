97e7c0a1fef736819db178b8fb18e3324a9f04e9
Kazuki Nakajima <nkjm@nkjm-macbook-air.local>
2017-04-24 15:12:12 +0900

    try adding product filter on rightnow search

--- service/rightnow.js
+++ service/rightnow.js
@@ -82,7 +82,7 @@ module.exports = class RightNow {
                                             LabelText:"LGBT",
                                         }]
                                     }
-                                ]}
+                                }]
                             }
                         }, function(err, result){
                             if (err){
